f8a610a259b1a69c30b8913e0a3b55f4da3c95d9
곽태욱 <litomi2026@gmail.com>
2026-05-24 16:00:47 +0800

    Update main.tf

--- infra/cloudflare/zone/litomi.in/bot-management/main.tf
+++ infra/cloudflare/zone/litomi.in/bot-management/main.tf
@@ -30,8 +30,7 @@ resource "cloudflare_bot_management" "default" {
   zone_id = var.zone_id
 
   ai_bots_protection      = "block"
-  cf_robots_variant       = "policy_only"
-  content_bots_protection = "block"
+  content_bots_protection = "disabled"
   crawler_protection      = "enabled"
   enable_js               = true
   fight_mode              = true
